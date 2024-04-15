3e2387ba1f6e1a6d8292adead5dadd76a5f08cdc
Michael Phillips <michaelphillips@vt.edu>
2024-04-14 21:14:20 -0400

    some style tweaks

--- front-end/src/js/Experience.js
+++ front-end/src/js/Experience.js
@@ -11,8 +11,8 @@ function Experience() {
                     <div className="ExperienceDescriptionSegment">
                         <img src={PeratonLogo} className="ExperienceCompanyLogo"></img>
                         <div className="ExperienceDescription">
-                            <p className="ExperienceCompanyText">Description: Full-time co-op serving the role of a full-stack software developer at Peraton. Whatever else we want to say and all that stuff</p>
-                            <p className="ExperienceCompanyText">May, 2023 - December, 2023</p>
+                            <p className="ExperienceCompanyText">Role: Full-Time Software Engineering Co-Op </p>
+                            <p className="ExperienceCompanyText">Employment Period: May, 2023 - December, 2023</p>
                         </div>
                     </div>
                 </div>

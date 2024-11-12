3247108e0edd7b3fd0b3d5844c48acbd159c503a
Michael Phillips <michaelphillips@vt.edu>
2024-11-11 22:14:16 -0500

    rotating border added. stil add little question mark at bottom for older versions

--- front-end/src/js/InfoCard.js
+++ front-end/src/js/InfoCard.js
@@ -1,11 +1,24 @@
 import headshot from '../images/headshot.jpg'
+import { useEffect, useState } from "react";
 
 function InfoCard() {
+
+    useEffect(() => {
+        let degree = 0;
+        let headshotContainer = document.getElementById("HeadshotContainer");
+        setInterval(() => {
+            degree = (degree + 1) % 360;
+            headshotContainer.style.background = `conic-gradient(from ${degree}deg, MediumSpringGreen, MediumSpringGreen, seagreen, teal, teal, seagreen, MediumSpringGreen, MediumSpringGreen  )`;
+        }, 10);
+    }, [])
+
     return <div id='InfoCardMain'>
-        <img id='Headshot' src={headshot}></img>
+        <div id='HeadshotContainer'>
+            <img id='Headshot' src={headshot}></img>
+        </div>
         <div id='DescriptorSegment'>
             <h2 class="section-title">Education</h2>
-            <h1 class="section-content">Virginia Tech - Aug 2022 to Dec 2025</h1>
+            <h1 class="section-content">Computer Science @ Virginia Tech (August 2022 to December 2025)</h1>
             <p>Currently pursuing a degree in computer science from Virginia Tech. Maintained a consistent 3.0+ GPA while simultaneously holding a greek life role as a brother of ΣN, ΘΞ691.</p>
 
             <h2 class="section-title">Previous Employment</h2>

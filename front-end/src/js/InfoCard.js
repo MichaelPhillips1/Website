f96b75def8bed69c9e36380faf4ca7a7ecfd7b5f
Michael Phillips <michaelphillips@vt.edu>
2024-11-29 20:27:59 -0500

    added my programming story

--- front-end/src/js/InfoCard.js
+++ front-end/src/js/InfoCard.js
@@ -27,6 +27,9 @@ function InfoCard() {
             <h1 class="section-content">Software Engineering Co-Op (Full-Stack) @ Peraton (May 2023 - December 2023)</h1>
             <p class="section-content">Performed under the role of Software Engineering Co-Op as a full-time employee from May, 2023 to December, 2023. Responsibilities included tasks such as user experience design, endpoint and API development, database architecture and development, data handling, responsive web design and device porting, regression testing, and many more full-stack duties.</p>
 
+            <h2 class="section-title">My Software Development Story</h2>
+            <h1 class="section-content">A Lifelong Love for Building and Creating</h1>
+            <p class="section-content">From a young age, I have been deeply passionate about all areas of computer programming and science, a field that has continuously fueled my curiosity and drive for innovation. My journey began at the age of 12 when I took my first coding class, and from that moment, I was hooked. I devoted countless hours to personal education, diving into a variety of programming languages and frameworks, and eagerly pursued formal coding classes throughout high school to expand my skill set. My interests span the entire spectrum of the field, from artificial intelligence and machine learning to front-end and back-end development, as well as user experience, user interface design, and graphic design. Every aspect of creating and improving software captivates me and gives me a profound sense of purpose. Programming isn’t just a skill or a career path for me—it’s the core of what drives me, challenges me, and inspires me to push boundaries. It’s my passion, my motivation, and what truly makes me tick.</p>
         </div>
     </div>
 }
